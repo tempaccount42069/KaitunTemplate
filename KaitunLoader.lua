f0e97ee16f7624e2fb0b895c7e4b33699a865e44
tempaccount42069 <mateohernandez2@proton.me>
2025-01-22 17:41:10 -0800

    Create KaitunLoader.lua

---
+++ KaitunLoader.lua
@@ -0,0 +1,11 @@
+-- Max level, godhuman, CDK
+getgenv().Configs = {
+    ["Team"] = "Pirates",
+    ["FPS Boost"] = true,
+    ["Farm Boss Drops"] = {
+        ["Enable"] = true,
+        ["When x2 Exp Expired"] = true
+    },
+    ["Hop Player Near"] = false
+}
+loadstring(game:HttpGet("http://apixerohub.x10.mx/kaitun.lua"))()
