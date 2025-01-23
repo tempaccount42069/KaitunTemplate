be5dbb04a0baf365a1cb18857f041cf09e55e72d
tempaccount42069 <mateohernandez2@proton.me>
2025-01-22 17:44:39 -0800

    Create MainLoader.lua

---
+++ MainLoader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KaitunTemplate/refs/heads/main/KaitunLoader.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KaitunTemplate/refs/heads/main/autochat.lua",true))()
