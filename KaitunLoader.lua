57963e2c857f88ac2fde80bdc0508a744b799247
tempaccount42069 <mateohernandez2@proton.me>
2025-01-22 18:08:58 -0800

    Update KaitunLoader.lua

--- KaitunLoader.lua
+++ KaitunLoader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KaitunTemplate/refs/heads/main/MainLoader.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KaitunTemplate/refs/heads/main/MainLoader.lua",true))(),
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KaitunTemplate/refs/heads/main/autochat.lua",true))()
